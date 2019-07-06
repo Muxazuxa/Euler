bdeab99ce0eaec1f1fe5e9fc314c1f6a35509c57
jenish <muxazuxa@mail.ru>
2019-07-07 00:28:17 +0600

    fix ignore

--- largest_palindrom.py
+++
@@ -1,9 +0,0 @@
-def palindrom():
-    for i in range(999, 99, -1):
-        for j in range(999, 99, -1):
-            m = i * j
-            if str(m) == str(m)[::-1]:
-                print(str(i) + '*' + str(j))
-                return str(i*j)
-
-print(palindrom())
--- sum_square_difference.py
+++
@@ -1,9 +0,0 @@
-def sum_diff():
-    sum1 = 0
-    sum2 = 0
-    for i in range(1,101):
-        sum1 += i*i
-        sum2 += i
-    return sum2 * sum2 - sum1
-
-print(sum_diff())
